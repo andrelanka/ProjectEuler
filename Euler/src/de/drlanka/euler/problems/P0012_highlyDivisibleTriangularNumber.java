df4016431bd34fc3d308272c483da37e06f8ee00
andrelanka <mail@dr-lanka.de>
2013-01-30 03:17:31 +0100

    Problem 0012

---
+++ Euler/src/de/drlanka/euler/problems/P0012_highlyDivisibleTriangularNumber.java
@@ -0,0 +1,26 @@
+package de.drlanka.euler.problems;
+
+import java.util.Map;
+
+import de.drlanka.euler.EulerProblem;
+import de.drlanka.euler.lib.Prime;
+
+public class P0012_highlyDivisibleTriangularNumber implements EulerProblem {
+
+  protected long divisorCount = 500;
+
+  @Override
+  public Object standardWay() {
+    long sum = 0;
+    for (long currentSummand = 1;; currentSummand++) {
+      sum += currentSummand;
+      Map<Long, Long> findPrimeFactorization = Prime.findPrimeFactorization(sum);
+      long divisors = 1;
+      for (Long multiplicity : findPrimeFactorization.values())
+        divisors *= multiplicity.longValue() + 1;
+      if (divisors > divisorCount)
+        return Long.valueOf(sum);
+    }
+  }
+
+}
