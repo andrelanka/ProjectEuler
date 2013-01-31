adeab534b512854ab4c710179660fc2b7d61a89f
andrelanka <mail@dr-lanka.de>
2013-01-31 09:05:52 +0100

    Re-created mvn project to obtain 'correct' directory structure.

---
+++ projectEuler/src/main/java/de/drlanka/App.java
@@ -0,0 +1,15 @@
+package de.drlanka;
+
+import de.drlanka.euler.Euler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      Euler.main(args);
+    }
+}
