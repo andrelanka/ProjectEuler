0fca8cb705d904fe021376c7607ad6d81849f9f4
andrelanka <mail@dr-lanka.de>
2013-05-24 07:43:42 +0200

    Just another minor change

--- projectEuler/src/test/java/de/drlanka/euler/problems/P0413oneChildNumbers_CacheGroupsTest.java
+++ projectEuler/src/test/java/de/drlanka/euler/problems/P0413oneChildNumbers_CacheGroupsTest.java
@@ -6,7 +6,7 @@ public class P0413oneChildNumbers_CacheGroupsTest extends TestCase {
   
   public void testCacheGroups() {
     P0413_oneChildNumbers_CacheGroups p0413 = new P0413_oneChildNumbers_CacheGroups();
-    p0413.currentLength=11;
+    p0413.currentLength=12;
     p0413.initialize();
     p0413.calculateEx0();
     p0413.extendEx0();
